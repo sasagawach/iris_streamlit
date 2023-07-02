89103df040597c987df471c646d5347ad1e12030
sasagawach <macy102e@gmail.com>
2023-07-02 15:02:17 +0900

    1st commmit

--- iris.py
+++ iris.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressions
 from sklearn.datasets import load_iris
 
 iris =  load_iris()
